1a0a0bf041c2d9ba226ff26916c7080f4a0e5b50
Ajay <najoku@ualberta.ca>
2022-10-06 19:34:10 -0600

    json

--- engine/instances/fireball.js
+++ engine/instances/fireball.js
@@ -1,28 +1,29 @@
 class Fireball extends EngineInstance {
-    onCreate(x, y, angle) {
-        this.x = x;
-        this.y = y;
-        this.angle = angle;
-        this.velocity = 10;
-        this.xScale = 0.2
-        this.yScale = 0.2
-        this.animation = [$engine.getTexture("fireball")]
-        this.sprite = $engine.createRenderable(this, new PIXI.extras.AnimatedSprite(this.animation), true)
-        this.setHitbox(new Hitbox(this, new RectangleHitbox(-160, -160, 160, 160)))
-    }
+	onCreate(x, y, angle) {
+		$engine.audioPlaySound("FireSoundEffect", 1.0, false, 0.5, 1.0);
+		this.x = x;
+		this.y = y;
+		this.angle = angle;
+		this.velocity = 10;
+		this.xScale = 0.2;
+		this.yScale = 0.2;
+		this.animation = [$engine.getTexture("fireball")];
+		this.sprite = $engine.createRenderable(this, new PIXI.extras.AnimatedSprite(this.animation), true);
+		this.setHitbox(new Hitbox(this, new RectangleHitbox(-160, -160, 160, 160)));
+	}
 
-    step() {
-        this.sprite.update(1)
-        this.x += Math.cos(this.angle) * this.velocity
-        this.y += Math.sin(this.angle) * this.velocity
-        
-        if(IM.instanceCollision(this, this.x, this.y, SolidObject)) {
-            this.destroy()
+	step() {
+		this.sprite.update(1);
+		this.x += Math.cos(this.angle) * this.velocity;
+		this.y += Math.sin(this.angle) * this.velocity;
 
-            var iceBlock = IM.instancePlace(this, this.x, this.y, IceBlock);
-            if (iceBlock !== undefined) {
-                iceBlock.destroy();
-            }
-        }
-    }
+		if (IM.instanceCollision(this, this.x, this.y, SolidObject)) {
+			this.destroy();
+
+			var iceBlock = IM.instancePlace(this, this.x, this.y, IceBlock);
+			if (iceBlock !== undefined) {
+				iceBlock.destroy();
+			}
+		}
+	}
 }
