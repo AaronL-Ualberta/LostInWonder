0c98db92e4b444d6da151f66f741983361525859
AaronL-Ualberta <ayl@ualberta.ca>
2022-10-21 23:36:08 -0600

    Limited lifespan of Fireball to be 10 seconds

--- engine/instances/fireball.js
+++ engine/instances/fireball.js
@@ -7,12 +7,18 @@ class Fireball extends EngineInstance {
 		this.velocity = 10;
 		this.xScale = 1;
 		this.yScale = 1;
+		this.lifespan = 600;	// Set the lifespan of the Fireball to be 10 seconds
 		this.animation = [$engine.getTexture("fireball")];
 		this.sprite = $engine.createRenderable(this, new PIXI.extras.AnimatedSprite(this.animation), true);
 		this.setHitbox(new Hitbox(this, new RectangleHitbox(-30, -20, 30, 20)));
 	}
 
 	step() {
+		this.lifespan--;
+		if (this.lifespan == 0) {
+			this.destroy();
+		}
+
 		this.sprite.update(1);
 		this.x += Math.cos(this.angle) * this.velocity;
 		this.y += Math.sin(this.angle) * this.velocity;
