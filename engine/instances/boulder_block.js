42f977550aa053143923e6c796690558ec2ac380
Nnamdi Ajoku <90708381+ajaycmput@users.noreply.github.com>
2022-10-23 19:27:56 -0600

    Boulder & Vine SE add.

--- engine/instances/boulder_block.js
+++ engine/instances/boulder_block.js
@@ -3,6 +3,7 @@ class BoulderBlock extends SolidObject {
 	onEngineCreate() {
 		this.setHitbox(new Hitbox(this, new RectangleHitbox(-24, -24, 24, 24)));
 		this.sprite = $engine.createRenderable(this, new PIXI.Sprite($engine.getTexture("boulder_block")), true);
+		this.isFalling = false;
 	}
 
 	onCreate(x, y) {
@@ -15,6 +16,11 @@ class BoulderBlock extends SolidObject {
 		var vineBlock = IM.instancePlace(this, this.x, this.y - 48, VineBlock);
 		// Start falling
 		if (vineBlock === undefined) {
+			if (!this.isFalling) {
+				// Boulder Drop Sound Effect
+				$engine.audioPlaySound("BoulderDropSoundEffect", 0.3, false);
+				this.isFalling = true;
+			}
 			var belowBlock = IM.instancePlace(this, this.x, this.y + 3, SolidObject);
 			if (belowBlock === undefined) {
 				this.y += 3;
